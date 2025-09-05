8e1dae81c25ac8be3efa84862c720da22bff89bf
rej <rzioma@acm.org>
2025-09-05 10:09:56 +0300

    Code cleanup and comments

--- src/vga_timing.v
+++ src/vga_timing.v
@@ -1,5 +1,43 @@
 `default_nettype none
 
+// VGA timing for 64 MHz base clock.
+//
+// This code is based on the previous implementations by RebelMike, hftab and ccattuto,
+// as well as informative discussions on TinyTapeout discord channels.
+
+// Graphics mode: 1024x768 60Hz CVT
+// 63.5 MHz pixel clock, rounded to 64 MHz
+// (courtesy of RebelMike)
+
+// Timing calculations were made with:
+//      https://tomverbeure.github.io/2019/08/03/Video-Timings-Calculator.html
+// Parameters:
+//      horiz_pixels=1024
+//      vert_pixels=768
+//      refresh_rate=60
+//      margins=false
+//      interlaced=false
+//      bpc=8
+//      color_fmt=rgb444
+//      video_opt=false
+//      custom_hblank=80
+//      custom_vblank=6
+
+
+// NOTES:
+// 1) SCREEN WIDTH. Although ~64 MHz clock provides standard 1024 clock / pixel wide screen,
+// 1024 wide does not suit lower 320 / 640 pixel resolutions - no simple divisor.
+// The closest divisable 960 can be achieved by adding 32 clock borders:
+//      (1024 - 960) / 2 = 32 additional border clocks
+// thus the optional 'narrow_960' mode.
+//
+// 2) SCANLINE COUNT. 64 MHz clock is slightly higher than 
+// the nominal 63.5 MHz pixel clock mandated by 1024x768 60Hz CVT.
+// As a stopgap solution extra scanlines can be added during the vertical blanking period:
+//      798 total vertical lines / 63.5 * 64 = 804 total vertical lines
+// thus the optional `extra_scanlines_for_64mhz` mode.
+
+
 module vga_timing (
     input wire clk,
     input wire rst_n,
@@ -17,33 +55,17 @@ module vga_timing (
     output reg interrupt
 );
 
-// 1024x768 60Hz CVT (63.5 MHz pixel clock, rounded to 64 MHz) - courtesy of RebelMike
-
-// `define H_ROLL   31
-// `define H_FPORCH (32 * 32)
-// `define H_SYNC   (33 * 32 + 16)
-// `define H_BPORCH (36 * 32 + 24)
-// `define H_NEXT   (41 * 32 + 15)
-
-// `define V_ROLL   47
-// `define V_FPORCH (16 * 64)
-// `define V_SYNC   (16 * 64 + 3)
-// `define V_BPORCH (16 * 64 + 7)
-// `define V_NEXT   (16 * 64 + 29)
 
 // 1024
 //  |          visible           |  blank |-=HSYNC=-|  blank  |
 //  |<---------- 1024 ---------->|<--48-->|<--104-->|<--151-->|
 //  0                            1024     1072      1176      1327
 
-// 960, additional blanks left&right 32 = (1024-960)/2
+// 960, additional blanks left & right 32 = (1024-960)/2
 //  |     visible    |     blank    |-=HSYNC=-|      blank    |
 //  |<----- 960 ---->|<-32-><--48-->|<--104-->|<--151--><-32->|
 //  0                960            1040      1144            1327
 
-//`define H_FPORCH 1024
-// `define H_SYNC   1072
-// `define H_BPORCH 1176
 `define H_FPORCH (narrow_960 ?  960 : 1024)
 `define H_SYNC   (narrow_960 ? 1040 : 1072)
 `define H_BPORCH (narrow_960 ? 1144 : 1176)
@@ -52,7 +74,6 @@ module vga_timing (
 `define V_FPORCH 768
 `define V_SYNC   771
 `define V_BPORCH 775
-`define V_NEXT   797 // 803 
 `define V_NEXT   (extra_vblank_lines_for_64mhz ? 803 : 797)
 
 always @(posedge clk) begin
@@ -91,46 +112,5 @@ always @(posedge clk) begin
 end
 
 assign blank = (x >= `H_FPORCH || y >= `V_FPORCH);
-
-// always @(posedge clk) begin
-//     if (!rst_n) begin
-//         x_hi <= 0;
-//         x_lo <= 0;
-//         y_hi <= 0;
-//         y_lo <= 0;
-//         hsync <= 0;
-//         vsync <= 0;
-//         interrupt <= 0;
-//     end else begin
-//         if ({x_hi, x_lo} == `H_NEXT) begin
-//             x_hi <= 0;
-//             x_lo <= 0;
-//         end else if (x_lo == `H_ROLL) begin
-//             x_hi <= x_hi + 1;
-//             x_lo <= 0;
-//         end else begin
-//             x_lo <= x_lo + 1;
-//         end
-//         if ({x_hi, x_lo} == `H_SYNC) begin
-//             if({y_hi, y_lo} == `V_NEXT) begin
-//                 y_hi <= 0;
-//                 y_lo <= 0;
-//                 interrupt <= 1;
-//             end else if (y_lo == `V_ROLL) begin
-//                 y_hi <= y_hi + 1;
-//                 y_lo <= 0;
-//             end else begin
-//                 y_lo <= y_lo + 1;
-//             end
-//         end
-//         hsync <= !({x_hi, x_lo} >= `H_SYNC && {x_hi, x_lo} < `H_BPORCH);
-//         vsync <= ({y_hi, y_lo} >= `V_SYNC && {y_hi, y_lo} < `V_BPORCH);
-//         if (cli || {y_hi, y_lo} == 0) begin
-//             interrupt <= 0;
-//         end
-//     end
-// end
-
-// assign blank = ({x_hi, x_lo} >= `H_FPORCH || {y_hi, y_lo} >= `V_FPORCH);
 
 endmodule
